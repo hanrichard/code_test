a71fc51a8731dffecfaba6a1f2b4d16dbe8cfef6
richardhan <hxy.314@gmail.com>
2019-10-31 22:25:14 +1100

    refactor code

--- src/components/Company.js
+++ src/components/Company.js
@@ -6,8 +6,44 @@ import Grid from '@material-ui/core/Grid';
 import CompanyItem from './CompanyItem';
 import Typography from '@material-ui/core/Typography';
 import Companyheader from './CompanyHeader';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Modal from '@material-ui/core/Modal';
+import { Paper } from '@material-ui/core';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
+import styled from 'styled-components';
 
 class Company extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { term: '', value: 'lastname', open: false };
+	}
+
+	handleInputChange = e => {
+		this.setState({
+			term: e.target.value,
+		});
+	};
+
+	handleChange = e => {
+		this.setState({
+			value: e.target.value,
+		});
+	};
+
+	handleOpen = () => {
+		this.setState({
+			open: true,
+		});
+	};
+
+	handleClose = () => {
+		this.setState({
+			open: false,
+		});
+	};
+
 	componentDidMount() {
 		this.props.onInitCompanies();
 	}
@@ -15,26 +51,86 @@ class Company extends Component {
 	render() {
 		const { companies } = this.props;
 		console.log(companies);
+		const currencies = [
+			{
+				value: 'lastname',
+				label: 'Last Name',
+			},
+			{
+				value: 'firstname',
+				label: 'First Name',
+			},
+		];
+
+		const Wrapper = styled.div`
+			.Company__bar {
+				margin-bottom: 30px;
+
+				@media only screen and (min-width: 600px) {
+					display: flex;
+					justify-content: space-between;
+					align-items: center;
+				}
+			}
+
+			.Company__bar-sortandsearch {
+				display: flex;
+			}
+			.Company__bar-sort {
+				margin-right: 10px;
+			}
+		`;
 
 		return companies.length === 0 ? (
 			<CircularProgress />
 		) : (
-			<div className="Company">
+			<Wrapper className="Company">
 				<div className="Company__header">
-					<Container maxWidth="md">
-						<Companyheader info={companies.companyInfo} />
-					</Container>
+					<Companyheader info={companies.companyInfo} />
 				</div>
 				<div className="Company__body">
 					<Container maxWidth="md">
-						<div>
-							<div>
+						<div className="Company__bar">
+							<div className="Company__bar-heading">
 								<Typography variant="h4" component="h2" gutterBottom>
 									Our Employees
 								</Typography>
 							</div>
-							<div>sort</div>
-							<div>input</div>
+							<div className="Company__bar-sortandsearch">
+								<div className="Company__bar-sort">
+									<TextField
+										id="outlined-select-currency"
+										select
+										label="Sort"
+										value={this.state.value}
+										onChange={e => this.handleChange(e)}
+										helperText="Sort by"
+										margin="normal"
+										variant="outlined"
+									>
+										{currencies.map(option => (
+											<MenuItem key={option.value} value={option.value}>
+												{option.label}
+											</MenuItem>
+										))}
+									</TextField>
+								</div>
+								<div className="Company__bar-search">
+									<form>
+										<TextField
+											id="outlined-search"
+											label="Search field"
+											type="search"
+											className=""
+											margin="normal"
+											variant="outlined"
+											value={this.state.term}
+											onChange={e => this.handleInputChange(e)}
+											helperText="Search field"
+										/>
+									</form>
+								</div>
+							</div>
 						</div>
 						<div>
 							<Grid container spacing={3}>
@@ -45,7 +141,30 @@ class Company extends Component {
 						</div>
 					</Container>
 				</div>
-			</div>
+
+				<button type="button" onClick={this.handleOpen}>
+					Open Modal
+				</button>
+
+				<Modal
+					aria-labelledby="simple-modal-title"
+					aria-describedby="simple-modal-description"
+					open={this.state.open}
+					onClose={this.handleClose}
+				>
+					<Container maxWidth="md">
+						<Paper>
+							<Paper p={3}>
+								<Card>
+									<CardContent>img</CardContent>
+
+									<CardContent>info</CardContent>
+								</Card>
+							</Paper>
+						</Paper>
+					</Container>
+				</Modal>
+			</Wrapper>
 		);
 	}
 }
